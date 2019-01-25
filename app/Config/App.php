e8f83d260586eb2c77e1e5a0e8d22d9b52a9f581
William Bradshaw <wbradshaw4@my.bcit.ca>
2019-01-25 10:53:37 -0800

    Modified base URL of project.

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'TrainerConnect';
+	public $baseURL = 'tc.local:4711';
 
 	/*
 	|--------------------------------------------------------------------------
